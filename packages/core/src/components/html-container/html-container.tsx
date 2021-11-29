d4313086c4dfe51c25181c048c843ebbeabff5c8
Noah Shipley <nshipley@centricsoftware.com>
2021-11-28 20:23:36 -0600

    Fix forwardRef usage in HTMLContainer

--- packages/core/src/components/html-container/html-container.tsx
+++ packages/core/src/components/html-container/html-container.tsx
@@ -5,8 +5,7 @@ interface HTMLContainerProps extends React.HTMLProps<HTMLDivElement> {
   children: React.ReactNode
 }
 
-export const HTMLContainer = observer(
-  React.forwardRef<HTMLDivElement, HTMLContainerProps>(function HTMLContainer(
+export const HTMLContainer = observer<HTMLContainerProps, HTMLDivElement>(function HTMLContainer(
     { children, ...rest },
     ref
   ) {
@@ -15,5 +14,4 @@ export const HTMLContainer = observer(
         {children}
       </div>
     )
-  })
-)
+  }, {forwardRef: true})
